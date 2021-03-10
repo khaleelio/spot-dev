b9ca877d625e90ee67c8a39930e11bf7ad0bf2de
Abdelrahman Hassan <abdo.hassn010@gmail.com>
2021-03-10 16:32:41 +0200

    Fix Issues

--- temp/j6j5JHr1tx/addons/client_addon/routes/clients.php
+++
@@ -1,9 +0,0 @@
-<?php 
-Route::group(['prefix' =>'admin', 'middleware' => ['auth', 'admin']], function(){
-	//Update Routes
-    Route::resource('clients','ClientController',[
-        'as' => 'admin'
-    ]);
-    Route::get('clients/delete/{client}','ClientController@destroy')->name('admin.clients.delete-client');
-
-});
